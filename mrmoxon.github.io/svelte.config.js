b5e6f0663d8fa92be4cbe8e8bd316c6a566d0266
Oscar Moxon <oscarjmoxon@gmail.com>
2024-03-08 20:51:58 +0000

    Deploy updated site

--- mrmoxon.github.io/svelte.config.js
+++ mrmoxon.github.io/svelte.config.js
@@ -1,16 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-import {
-    vitePreprocess
-} from '@sveltejs/kit/vite';
+import preprocess from 'svelte-preprocess'; // Import svelte-preprocess
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+    // If you're using preprocessors like TypeScript or SCSS
+    preprocess: preprocess(), // Use preprocess() here
 
     kit: {
         adapter: adapter(),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+            base: process.env.NODE_ENV === 'production' ? '/mrmoxon.github.io' : '',
         }
     }
 };
